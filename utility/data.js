3998bd9ff51e7c53e341ae51ff6f4f28a3c3ba82
Natasha <njohnson533@gmail.com>
2022-04-21 15:36:44 -0400

    removed good faith and no suprises footer links

--- utility/data.js
+++ utility/data.js
@@ -52,18 +52,9 @@ export const footerLinks = [
     url: "/contact",
     text: "Contact",
   },
-
   {
     url: "/#mission",
     text: "Mission",
-  },
-  {
-    url: "/#goodfaith",
-    text: "Good Faith Estimate",
-  },
-  {
-    url: "/#nosuprises",
-    text: "No Surprises Act",
   },
   {
     url: "https://www.psychologytoday.com/us/therapists/dr-selisha-nelson-north-chesterfield-va/179620",
